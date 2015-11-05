c8146222277db1759472903d69f44162758da204
Chevi <chevilopez@gmail.com>
2015-11-05 07:35:31 -0500

    continuando con djk

--- src/tallerVoraces/Dijkstra.java
+++ src/tallerVoraces/Dijkstra.java
@@ -23,38 +23,62 @@ public class Dijkstra {
 
     public void dijkstra(HashMap<String, LinkedList<String>> mapa, String cadena) {
         String cad = cadena;
+        String sel;
         LinkedList<String> solucion = new LinkedList<>();
-        System.out.println("-" + cad);
+        //System.out.println("-" + cad);
         matriz = new String[mapa.size()][mapa.size() + 1];
+        //System.out.println("size: " + mapa.size() + " -- length " + matriz.length);
+
         //----------------llendo la matriz
         Iterator it = mapa.keySet().iterator();
         int i = 0;
         while (it.hasNext()) {
             String key = (String) it.next();
-            matriz[0][i] = key;
+            matriz[i][0] = key;
             i++;
         }
-        int cont = 1; // Numero del a columna sobre la que voy iterando  
-        //while (cont < mapa.size()) {
-        for (int j = 0; j < matriz[0].length - 1; j++) {
-            if (matriz[0][j].equals(cad)) {
-                matriz[cont][j] = 0 + "," + cad + "," + "*";
-            }
-            for (String get1 : adyacentes(mapa, cad)) {
-                if (matriz[0][j].equals(get1.split(",")[0]) && matriz[cont][j] == null) {
-                    matriz[cont][j] = get1.split(",")[1] + "," + cad;
+
+        int iter = 1; // Numero del a columna sobre la que voy iterando  
+        while (iter < mapa.size()) {
+            for (int j = 0; j < matriz[0].length - 1; j++) {
+                if (cad.length() == 1) {
+                    if (matriz[j][0].equals(cad)) {
+                        matriz[j][iter] = 0 + "," + cad + "," + "*";
+                    }
+
+                    for (String get1 : adyacentes(mapa, cad)) {
+                        if (matriz[j][0].equals(get1.split(",")[0]) && matriz[j][iter] == null) {
+                            matriz[j][iter] = get1.split(",")[1] + "," + cad;
+                        }
+                    }
+
+                } else {
+                    if (matriz[j][0].equals(cad.split(",")[0])) {
+                        matriz[j][iter] = cad.split(",")[1] + "," + cad.split(",")[0] + "," + "*";
+                    }
                 }
+
+                for (String get1 : adyacentes(mapa, cad.split(",")[0])) {
+                    if (matriz[j][0].equals(get1.split(",")[0]) && matriz[j][iter] == null) {
+                        matriz[j][iter] = get1.split(",")[1] + "," + cad.split(",")[0];
+                    }
+                }
+                marcar(matriz);
             }
+            sel = seleccion(matriz, iter);
+            cad = sel;
+            System.out.println("seleccionado: " + sel);
+            iter++;
         }
-        String sel = seleccion(matriz, cont);
         for (int j = 0; j < matriz[0].length - 1; j++) {
-            for (int k = 0; k < matriz[1].length - 1; k++) {
-                System.out.print(matriz[k][j] + "|    ");
+            for (int k = 0; k < matriz[1].length; k++) {
+                System.out.print(matriz[j][k] + "|    ");
             }
             System.out.println("");
         }
         //}
     }
+
     // obtengo los adyacentes de "cad(a)"
     public LinkedList<String> adyacentes(HashMap<String, LinkedList<String>> mapa, String cadena) {
         LinkedList<String> listAdyacentes = new LinkedList<>();
@@ -64,24 +88,36 @@ public class Dijkstra {
         return listAdyacentes;
     }
 
+    public void marcar(String[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 1; j < m[0].length; j++) {
+                if (m[i][j] != null && m[i][j].split(",").length > 2 && m[i][j].split(",")[2].equals("*")) {
+                    for (int k = j + 1; k < m[0].length; k++) {
+                        m[i][k] = "*";
+                    }
+                }
+            }
+        }
+    }
+
     public boolean factible(HashMap<String, LinkedList<String>> mapa, String x) {
         return false;
     }
 
     public String seleccion(String matriz[][], int cont) {
         //System.out.println("+++++++ "+matriz.length);
-        String sel = "100";
+
+        String sel = "";
         int x = 100;
         for (int i = 0; i < matriz.length; i++) {
-            System.out.println("´´´´´´´´´´´" + matriz[cont][i]);
-            if (!matriz[cont][i].split(",")[2].equals("*")) {
-                if (Integer.parseInt(matriz[cont][i].split(",")[0]) < x) {
-                    x = Integer.parseInt(matriz[cont][i].split(",")[0]);
+            if (matriz[i][cont] != null && matriz[i][cont].split(",").length < 3) {
+                if (Integer.parseInt(matriz[i][cont].split(",")[0]) < x) {
+                    x = Integer.parseInt(matriz[i][cont].split(",")[0]);
+                    sel = matriz[i][0] + "," + matriz[i][cont];
                 }
             }
         }
-
-        return null;
+        return sel;
     }
 
     public boolean solucion(HashMap s) {
